4a063018d5d6bf8316f1afaa8976d5351392af43
Iana Gamurar <iana_gamurari@bk.ru>
2019-07-18 11:55:57 +0300

    add catalog page

---
+++ src/main/webapp/app/catalog/catalog.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { IBook } from 'app/shared/model/book.model';
+import { BookService} from "app/entities/book";
+import {HttpErrorResponse, HttpResponse} from "@angular/common/http";
+
+@Component({
+  selector: 'jhi-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.scss']
+})
+export class CatalogComponent implements OnInit {
+  books: IBook[];
+
+  constructor(private bookService: BookService) { }
+
+  loadAll() {
+    this.bookService
+      .query({
+        page: 0,
+        size: 10
+      })
+      .subscribe(
+        (res: HttpResponse<IBook[]>) => this.books = res.body,
+        (res: HttpErrorResponse) => console.error(res.message)
+      );
+  }
+
+  ngOnInit() {
+    this.loadAll();
+  }
+
+}
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -3,10 +3,14 @@ import { RouterModule } from '@angular/router';
 
 import { LibrarySharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
+import { CatalogComponent } from "app/catalog/catalog.component";
 
 @NgModule({
   imports: [LibrarySharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    CatalogComponent
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LibraryHomeModule {}
